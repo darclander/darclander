37d991f12a13ec3de2cbfeb38f40725e90313e9e
darclander <lukedarclander@gmail.com>
2026-01-22 21:15:47 +0100

    add default nvim config

---
+++ config/nvim/lua/core/config.lua
@@ -0,0 +1,33 @@
+-- Enable syntax highlighting and filetype plugins
+vim.cmd("syntax on")
+vim.cmd("filetype plugin indent on")
+
+-- Line numbers
+vim.opt.number = true
+-- vim.opt.relativenumber = true
+
+-- Matching brackets
+vim.opt.showmatch = true
+
+-- Tabs & indentation
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+-- Search behavior
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = false
+
+-- Command-line completion
+vim.opt.wildmenu = true
+
+-- Backspace behavior
+vim.opt.backspace = { "indent", "eol", "start" }
+
+-- Line wrapping
+vim.opt.wrap = true
+
