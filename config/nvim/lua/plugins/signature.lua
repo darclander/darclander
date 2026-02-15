fcf6dfa19c30b60137012964ae7e24b2dedf5bf1
darclander <lukedarclander@gmail.com>
2026-02-15 20:57:13 +0100

    update nvim config

--- config/nvim/lua/plugins/signature.lua
+++ config/nvim/lua/plugins/signature.lua
@@ -1,26 +1,28 @@
 return {
-  "ray-x/lsp_signature.nvim",
-  event = "InsertEnter",
-  config = function()
-    require("lsp_signature").setup({
-      bind = true,
-      floating_window = true,
-      floating_window_above_cur_line = false,
-      fix_pos = false,
-      hint_enable = false,
-      hint_prefix = "󰌵 ",
-      hint_scheme = "String",
-      max_height = 4,
-      max_width = 80,
-      doc_lines = 2,
-      handler_opts = {
-        border = "rounded",
-        zindex = 200,
-      },
-      extra_trigger_chars = { "(", "," },
-      toggle_key = "<M-x>",
-      active_parameter = "PmenuSel",
-    })
-  end,
+	"ray-x/lsp_signature.nvim",
+	event = "InsertEnter",
+	config = function()
+		require("lsp_signature").setup({
+			max_height = 4,
+			max_width = 60,
+			doc_lines = 4,
+			handler_opts = {
+				border = "rounded",
+				zindex = 200,
+			},
+			wrap = true,
+			bind = true,
+			floating_window = true,
+			floating_window_above_cur_line = false,
+			fix_pos = false,
+			hint_enable = false,
+			hint_prefix = "󰌵 ",
+			hint_scheme = "String",
+
+			extra_trigger_chars = { "(", "," },
+			toggle_key = "<M-x>",
+			active_parameter = "PmenuSel",
+		})
+	end,
 }
 
